d43be50145cf2f9348740fd753750e178bf71c2c
Mike Drake <Drake620@gmail.com>
2026-02-15 17:52:23 -0700

    Initial Commit

---
+++ PortPeek/PortPeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortPeekApp.swift
+//  PortPeek
+//
+//  Created by Michael Drake on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct PortPeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
